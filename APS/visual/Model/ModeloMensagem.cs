1083e41f6ffe8fbff2c3faea5e978b5b7abec557
DaviGF02 <davigf2002@gmail.com>
2022-05-12 17:16:01 -0300

    interface pronta

---
+++ APS/visual/Model/ModeloMensagem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APS.visual.Model
+{
+    internal class ModeloMensagem
+    {
+        public string UserName { get; set; }
+        public string UserNameColor { get; set; }
+        public string ImageSource { get; set; }
+        public string Message { get; set; }
+        public DateTime Time { get; set; }
+        public bool IsNativeorigin { get; set; }
+        public bool? FirstMessage { get; set; }
+
+    }
+}
--- APS/visual/ViewModel/MainViewModel.cs
+++ APS/visual/ViewModel/MainViewModel.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using APS.visual.Model;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,82 @@ namespace APS.MWM.ViewModel
 {
     internal class MainViewModel
     {
+        public ObservableCollection<ModeloMensagem> Mensagens { get; set; }
+
+        public ObservableCollection<ModeloContatos> Contatos { get; set; }
+
+        public MainViewModel()
+        {
+            Mensagens = new ObservableCollection<ModeloMensagem>();
+
+            Contatos = new ObservableCollection<ModeloContatos>();
+
+            Mensagens.Add(new ModeloMensagem
+            {
+                UserName = "Davi",
+                UserNameColor = "#409AFF",
+                ImageSource = "https://imgur.com/gallery/EFeEbuJ",
+                Message = "Teste",
+                Time = DateTime.Now,
+                IsNativeorigin = false,
+                FirstMessage = true
+
+            });
+
+            for (int i = 0; i < 3; i++)
+            {
+                Mensagens.Add(new ModeloMensagem
+                {
+                    UserName = "Lucas",
+                    UserNameColor = "#409AFF",
+                    ImageSource = "https://imgur.com/gallery/EFeEbuJ",
+                    Message = "Teste",
+                    Time = DateTime.Now,
+                    IsNativeorigin = false,
+                    FirstMessage = false
+
+                });
+
+            }
+
+            for (int i = 0; i < 4; i++)
+            {
+                Mensagens.Add(new ModeloMensagem
+                {
+                    UserName = "Vanessa",
+                    UserNameColor = "#409AFF",
+                    ImageSource = "https://imgur.com/gallery/EFeEbuJ",
+                    Message = "Teste",
+                    Time = DateTime.Now,
+                    IsNativeorigin = true,
+
+                });
+
+            }
+
+            Mensagens.Add(new ModeloMensagem
+            {
+                UserName = "Luan",
+                UserNameColor = "#409AFF",
+                ImageSource = "https://imgur.com/gallery/EFeEbuJ",
+                Message = "Ultima",
+                Time = DateTime.Now,
+                IsNativeorigin = true,
+
+            });
+
+            for (int i = 0; i < 5; i++)
+            {
+                Contatos.Add(new ModeloContatos
+                {
+                    UserName = $"Adryan {i}",
+                    SourceImage = "https://imgur.com/gallery/cNpdfmy",
+                    mensagens = Mensagens
+                });
+            }
+
+
+        }
+       
     }
 }
